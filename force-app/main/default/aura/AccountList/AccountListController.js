d38f7d4fe85366f263fd43709b5eba8d4d137327
Natalie Lorenzetti <neb2015@gmail.com>
2018-03-22 14:32:26 -0700

    Adding pinpoints to the map

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -2,8 +2,11 @@
     doInit : function(component, event) {
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
-          component.set("v.accounts", a.getReturnValue()); 
+            component.set("v.accounts", a.getReturnValue());
+            var event2 = $A.get("e.c:AccountsLoaded");
+            event2.setParams({"accounts": a.getReturnValue()});
+            event2.fire();
         });
-        $A.enqueueAction(action);
+    $A.enqueueAction(action);
     }
 })
